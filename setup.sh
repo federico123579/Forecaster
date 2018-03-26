abaa03e2e182ff71310575aa7cc86d397cad483b
Federico Lolli <federico123579@gmail.com>
2018-03-26 21:47:04 +0200

    Deleted setup

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -p "Telegram token: " telegram_token
-read -p "Sentry token: " sentry_token
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-token_file="$DIR/forecaster/data/tokens.yml"
-touch $token_file
-echo "telegram: $telegram_token" > $token_file
-echo "sentry: $sentry_token" >> $token_file
-echo tokens saved
