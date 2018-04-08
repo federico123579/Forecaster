dae8524290439ef2fd16e294b0c07dcb3c51062f
Federico Lolli <federico123579@gmail.com>
2018-04-08 10:59:50 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         'scipy',
         'pyyaml',
-        'trading212-api',
+        'trading212api',
         'python-telegram-bot',
         'raven',
         'termcolor',
